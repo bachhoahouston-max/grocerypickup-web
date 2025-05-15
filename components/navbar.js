446b3923be5efa9ec9991a82bf048121f70d27cd
Rishabh tiwari <rishabhtiwari73096@gmail.com>
2025-05-15 18:37:21 +0530

    update

--- components/navbar.js
+++ components/navbar.js
@@ -454,32 +454,33 @@ const Navbar = (props) => {
         console.log(newData)
         localStorage.setItem("checkoutData", JSON.stringify(newData));
         props.loader && props.loader(true);
-        Api("post", "createProductRquest", newData, router).then(
-            (res) => {
-                props.loader && props.loader(false);
-                if (res.status) {
-                    setCartData([]);
-                    setLocalAddress([])
-                    setCartTotal(0);
-                    setOpenCart(false);
-                    setDate('')
-                    getProfileData()
-                    localStorage.removeItem("addCartDetail");
-                    router.push("/Mybooking");
-                    props.toaster({ type: "success", message: "Thank you for your order! Your item will be processed shortly." });
+        
+        // Api("post", "createProductRquest", newData, router).then(
+        //     (res) => {
+        //         props.loader && props.loader(false);
+        //         if (res.status) {
+        //             setCartData([]);
+        //             setLocalAddress([])
+        //             setCartTotal(0);
+        //             setOpenCart(false);
+        //             setDate('')
+        //             getProfileData()
+        //             localStorage.removeItem("addCartDetail");
+        //             router.push("/Mybooking");
+        //             props.toaster({ type: "success", message: "Thank you for your order! Your item will be processed shortly." });
                     
-                } else {
-                    props.toaster && props.toaster({ type: "error", message: res?.data?.message });
-                }
-            },
-            (err) => {
-                props.loader && props.loader(false);
-                props.toaster && props.toaster({ type: "error", message: err?.message });
-            }
-        );
+        //         } else {
+        //             props.toaster && props.toaster({ type: "error", message: res?.data?.message });
+        //         }
+        //     },
+        //     (err) => {
+        //         props.loader && props.loader(false);
+        //         props.toaster && props.toaster({ type: "error", message: err?.message });
+        //     }
+        // );
        
-        // setOpenCart(false)
-        // router.push('/payment?from=cart')
+        setOpenCart(false)
+        router.push('/payment?from=cart')
 
     };
 
