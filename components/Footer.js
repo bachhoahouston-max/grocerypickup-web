d0960d34dd20d4c876af1bf3327b470131d6f6a7
Rishabh-2digit <rishabhtiwari73096@gmail.com>
2025-07-11 16:27:50 +0530

    update

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from "react";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 import { IoCall } from "react-icons/io5";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import { MdEmail } from "react-icons/md";
 import { useTranslation } from "react-i18next";
 
-
 const Footer = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
   const router = useRouter();
   return (
     <footer className="p-10 bg-custom-green text-black font-sans">
@@ -25,78 +24,193 @@ const Footer = () => {
               height="50"
               src="/Logo2.png"
               width="150"
-              onClick={() => router.push('/')}
+              onClick={() => router.push("/")}
             />
             <p className="text-[16px] md:text-[19px] text-white">
-              {t("Your digital partner for growth, with a commitment to quality")} </p>
+              {t(
+                "Your digital partner for growth, with a commitment to quality"
+              )}{" "}
+            </p>
             <div className="flex space-x-4 mt-4">
-             
-              <a className="text-white border-2 rounded-full w-[40px] h-[40px] border-white flex justify-center items-center transition-transform duration-300 hover:-translate-y-[8px] " href="https://www.instagram.com/bachhoahouston/">
+              <a
+                className="text-white border-2 rounded-full w-[40px] h-[40px] border-white flex justify-center items-center transition-transform duration-300 hover:-translate-y-[8px] "
+                href="https://www.instagram.com/bachhoahouston/"
+              >
                 <FaInstagram />
               </a>
-              <a className="text-white border-2 rounded-full w-[40px] h-[40px] border-white flex justify-center items-center transition-transform duration-300 hover:-translate-y-[8px]" href="https://www.facebook.com/groups/1861501697993458">
+              <a
+                className="text-white border-2 rounded-full w-[40px] h-[40px] border-white flex justify-center items-center transition-transform duration-300 hover:-translate-y-[8px]"
+                href="https://www.facebook.com/groups/1861501697993458"
+              >
                 <FaFacebookF />
               </a>
-           
             </div>
-            <div className='mt-6 text-white'> <ul>
-              <li className="flex items-center mb-2 transition-transform duration-300 hover:-translate-y-[8px]">
-                <IoCall className="text-2xl mr-2" />
-                <a href="tel:832-230-9288" className="text-[16px] md:text-[19px]text-white cursor-pointer">832-230-9288</a>
-              </li>
-              <li className="flex items-center transition-transform duration-300 hover:-translate-y-[8px]">
-                <MdEmail className="text-2xl mr-2" />
-                <a className="text-[16px] md:text-[19px]" href="mailto:contact@bachhoahouston.com">contact@bachhoahouston.com</a>
-              </li>
-            </ul></div>
+            <div className="mt-6 text-white">
+              {" "}
+              <ul>
+                <li className="flex items-center mb-2 transition-transform duration-300 hover:-translate-y-[8px]">
+                  <IoCall className="text-2xl mr-2" />
+                  <a
+                    href="tel:832-230-9288"
+                    className="text-[16px] md:text-[19px]text-white cursor-pointer"
+                  >
+                    832-230-9288
+                  </a>
+                </li>
+                <li className="flex items-center transition-transform duration-300 hover:-translate-y-[8px]">
+                  <MdEmail className="text-2xl mr-2" />
+                  <a
+                    className="text-[16px] md:text-[19px]"
+                    href="mailto:contact@bachhoahouston.com"
+                  >
+                    contact@bachhoahouston.com
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
 
-          <div className='flex flex-col md:flex-row w-full text-white'>
+          <div className="flex flex-col md:flex-row w-full text-white">
             {/* Useful Links */}
             <div className="w-full md:w-1/3 mb-4 md:mb-0 ">
-              <h3 className="font-bold mb-4  lg:text-[19px] text-[18px]">{t("Useful Links")}</h3>
+              <h3 className="font-bold mb-4  lg:text-[19px] text-[18px]">
+                {t("Useful Links")}
+              </h3>
               <ul>
-                <li className="md:pb-2 pb-1 transition-transform duration-300 hover:-translate-y-[8px] hover:underline"><a className="text-[16px] md:text-[17px] " href="" onClick={() => router.push('/')}> {t("Home")}</a></li>
-                <li className="md:pb-2 pb-1 transition-transform duration-300 hover:-translate-y-[8px] hover:underline"><a className="cursor-pointer text-[16px] md:text-[17px]" onClick={() => router.push('/AboutUs')}>{t("About Us")} </a></li>
-                <li className="md:pb-2 pb-1 transition-transform duration-300 hover:-translate-y-[8px] hover:underline"><a className="cursor-pointer text-[16px] md:text-[17px]" onClick={() => router.push('/ContactUs')}>{t("Contact")}</a></li>
+                <li className="md:pb-2 pb-1 transition-transform duration-300 hover:-translate-y-[8px] hover:underline">
+                  <a
+                    className="text-[16px] md:text-[17px] "
+                    href=""
+                    onClick={() => router.push("/")}
+                  >
+                    {" "}
+                    {t("Home")}
+                  </a>
+                </li>
+                <li className="md:pb-2 pb-1 transition-transform duration-300 hover:-translate-y-[8px] hover:underline">
+                  <a
+                    className="cursor-pointer text-[16px] md:text-[17px]"
+                    onClick={() => router.push("/AboutUs")}
+                  >
+                    {t("About Us")}{" "}
+                  </a>
+                </li>
+                <li className="md:pb-2 pb-1 transition-transform duration-300 hover:-translate-y-[8px] hover:underline">
+                  <a
+                    className="cursor-pointer text-[16px] md:text-[17px]"
+                    onClick={() => router.push("/ContactUs")}
+                  >
+                    {t("Contact")}
+                  </a>
+                </li>
+
+                <li className="md:pb-2 pb-1 transition-transform duration-300 hover:-translate-y-[8px] hover:underline">
+                  <a
+                    className="cursor-pointer text-[16px] md:text-[17px]"
+                    onClick={() => router.push("/StoreLocation")}
+                  >
+                    {t("Store Location")}
+                  </a>
+                </li>
+                <li className="md:pb-2 pb-1 transition-transform duration-300 hover:-translate-y-[8px] hover:underline">
+                  <a
+                    className="cursor-pointer text-[16px] md:text-[17px]"
+                    onClick={() => router.push("/FranchiseOpportunity")}
+                  >
+                    {t("Franchise Opportunity")}
+                  </a>
+                </li>
               </ul>
             </div>
 
             {/* Help & Support */}
             <div className="w-full md:w-1/3 mb-4 md:mb-0">
-              <h3 className="font-bold mb-4  lg:text-[19px] text-[18px]">{t("Help & Support")}</h3>
+              <h3 className="font-bold mb-4  lg:text-[19px] text-[18px]">
+                {t("Help & Support")}
+              </h3>
               <ul>
-                <li className="pb-2 cursor-pointer transition-transform duration-300 hover:-translate-y-[8px] hover:underline"><a className="text-[16px] md:text-[17px]" onClick={() => router.push('/ReturnPolicy')}>{t("Return Policy")} </a></li>
-                <li className="pb-2 cursor-pointer transition-transform duration-300 hover:-translate-y-[8px] hover:underline"><a className="text-[16px] md:text-[17px]" onClick={() => router.push('/Termsandcondition')}>{t("Terms and Conditions")} </a></li>
-               
-                <li className="pb-2 cursor-pointer transition-transform duration-300 hover:-translate-y-[8px] hover:underline"><a className="text-[16px] md:text-[17px]" onClick={() => router.push('/PrivacyPolicy')}>{t("Privacy Policy")} </a></li>
-                
+                <li className="pb-2 cursor-pointer transition-transform duration-300 hover:-translate-y-[8px] hover:underline">
+                  <a
+                    className="text-[16px] md:text-[17px]"
+                    onClick={() => router.push("/ReturnPolicy")}
+                  >
+                    {t("Return Policy")}{" "}
+                  </a>
+                </li>
+                <li className="pb-2 cursor-pointer transition-transform duration-300 hover:-translate-y-[8px] hover:underline">
+                  <a
+                    className="text-[16px] md:text-[17px]"
+                    onClick={() => router.push("/Termsandcondition")}
+                  >
+                    {t("Terms and Conditions")}{" "}
+                  </a>
+                </li>
+
+                <li className="pb-2 cursor-pointer transition-transform duration-300 hover:-translate-y-[8px] hover:underline">
+                  <a
+                    className="text-[16px] md:text-[17px]"
+                    onClick={() => router.push("/PrivacyPolicy")}
+                  >
+                    {t("Privacy Policy")}{" "}
+                  </a>
+                </li>
               </ul>
             </div>
-      
 
-          
             {/* Other Links */}
             <div className="w-full md:w-1/3 mb-4 md:mb-0">
-              <h3 className="font-bold mb-4  lg:text-[19px] text-[18px]">{("Other Links")}</h3>
+              <h3 className="font-bold mb-4  lg:text-[19px] text-[18px]">
+                {"Other Links"}
+              </h3>
               <ul>
-                <li className="md:pb-2 pb-1 cursor-pointer transition-transform duration-300 hover:-translate-y-[8px] hover:underline"><a className="text-[16px] md:text-[17px]" onClick={() => router.push('/Myhistory')}>{t("History")}</a></li>
+                <li className="md:pb-2 pb-1 cursor-pointer transition-transform duration-300 hover:-translate-y-[8px] hover:underline">
+                  <a
+                    className="text-[16px] md:text-[17px]"
+                    onClick={() => router.push("/Myhistory")}
+                  >
+                    {t("History")}
+                  </a>
+                </li>
 
-                <li className="md:pb-2 pb-1 cursor-pointer transition-transform duration-300 hover:-translate-y-[8px] hover:underline"><a className="text-[16px] md:text-[17px]" onClick={() => router.push('/Mybooking')}>{t("My Order")}</a></li>
+                <li className="md:pb-2 pb-1 cursor-pointer transition-transform duration-300 hover:-translate-y-[8px] hover:underline">
+                  <a
+                    className="text-[16px] md:text-[17px]"
+                    onClick={() => router.push("/Mybooking")}
+                  >
+                    {t("My Order")}
+                  </a>
+                </li>
 
-                 <li className="md:pb-2 pb-1 cursor-pointer transition-transform duration-300 hover:-translate-y-[8px] hover:underline"><a className="text-[16px] md:text-[17px]" onClick={() => router.push('/JoinOurDelievryTeam')}>{t(" Join our delivery team")}</a></li>
+                <li className="md:pb-2 pb-1 cursor-pointer transition-transform duration-300 hover:-translate-y-[8px] hover:underline">
+                  <a
+                    className="text-[16px] md:text-[17px]"
+                    onClick={() => router.push("/JoinOurDelievryTeam")}
+                  >
+                    {t(" Join our delivery team")}
+                  </a>
+                </li>
 
-                <li className="md:pb-2 pb-1 cursor-pointer transition-transform duration-300 hover:-translate-y-[8px] hover:underline"><a className="text-[16px] md:text-[17px]" 
-                onClick={() => router.push('/HelpCenter')}>{t("⁠Help Center")}</a></li>
+                <li className="md:pb-2 pb-1 cursor-pointer transition-transform duration-300 hover:-translate-y-[8px] hover:underline">
+                  <a
+                    className="text-[16px] md:text-[17px]"
+                    onClick={() => router.push("/HelpCenter")}
+                  >
+                    {t("⁠Help Center")}
+                  </a>
+                </li>
 
-                 <li className="md:pb-2 pb-1 cursor-pointer transition-transform duration-300 hover:-translate-y-[8px] hover:underline"><a className="text-[16px] md:text-[17px]" onClick={() => router.push('/ProductRecallInfo')}>{t("⁠Product Recall Information")}</a></li>
-
+                <li className="md:pb-2 pb-1 cursor-pointer transition-transform duration-300 hover:-translate-y-[8px] hover:underline">
+                  <a
+                    className="text-[16px] md:text-[17px]"
+                    onClick={() => router.push("/ProductRecallInfo")}
+                  >
+                    {t("⁠Product Recall Information")}
+                  </a>
+                </li>
               </ul>
             </div>
-            </div>
-          
+          </div>
         </div>
-
       </div>
 
       <div className="flex justify-center pb-12 md:pb-6">
@@ -106,14 +220,15 @@ const Footer = () => {
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
 
-
-
-  {/* Contact Us */}
-            {/* <div className="w-full md:w-1/2 mb-4 md:mb-0">
+{
+  /* Contact Us */
+}
+{
+  /* <div className="w-full md:w-1/2 mb-4 md:mb-0">
               <h3 className="font-bold mb-4  lg:text-[19px] text-[18px]">{t("Contact Us")}</h3>
               <ul>
                 <li className="flex items-center mb-2">
@@ -125,4 +240,5 @@ export default Footer;
                   <a className="text-[16px] md:text-[19px]" href="mailto:contact@bachhoahouston.com">contact@bachhoahouston.com</a>
                 </li>
               </ul>
-            </div> */}
+            </div> */
+}
