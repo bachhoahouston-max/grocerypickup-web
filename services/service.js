71863f6e2546c0b61fe3cab739de265bb6b4b919
Rishabh-2digit <rishabhtiwari73096@gmail.com>
2025-08-25 15:57:29 +0530

    update work 25-08-2025

--- services/service.js
+++ services/service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const ConstantsUrl = "http://localhost:3004/v1/api/";
-// const ConstantsUrl = "https://api.bachhoahouston.com/v1/api/";
+// const ConstantsUrl = "http://localhost:3004/v1/api/";
+const ConstantsUrl = "https://api.bachhoahouston.com/v1/api/";
  
 function Api(method, url, data, router, params) {
   return new Promise(function (resolve, reject) {
